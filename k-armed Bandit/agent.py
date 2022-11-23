c2a0827483ff69ad3030b53828b1b766382fcf7f
Artem Kryukov <kryukov.aa@phystech.edu>
2022-11-23 15:47:30 +0300

    Gradient Bandit

--- k-armed Bandit/agent.py
+++ k-armed Bandit/agent.py
@@ -1,6 +1,8 @@
 from gymnasium import Env
 import numpy as np
+import numpy.typing as npt
 from collections import defaultdict
+from scipy.special import softmax
 
 
 class SimpleAgent():
@@ -76,3 +78,42 @@ class UCBSimpleAgent(SimpleAgent):
         ucb_value = self.q_values[observation]+ self.ucb_c * np.sqrt(np.log(self.n_step)/self.times[observation])
         action = int(np.argmax(ucb_value))
         return action
+    
+class GradientAgent():
+    def __init__(self, environment: Env, **kwargs):
+        self.env = environment
+        self.avg_reward = 0
+        self.preferences = defaultdict(lambda: np.zeros((self.env.action_space.n), dtype=np.float64))
+        self.n_step = 0
+        
+        self.baseline = False
+        if  "baseline" in kwargs:
+            self.baseline= kwargs["baseline"]
+        if "step_size" in kwargs:
+            self.step_size = kwargs["step_size"]
+        else:
+            self.step_size = 0.1
+
+    def _get_action_probability(self, observation: int) -> npt.NDArray[np.float64]:
+        return softmax(self.preferences[observation])
+    
+    def get_action(self, observation: int) -> int:
+        probabilities = self._get_action_probability(observation)
+        action = np.random.choice(self.env.action_space.n, p =probabilities)
+        return action
+
+    def update(
+        self,
+        observation: int,
+        action: int,
+        reward: float,
+        terminated: bool = False,
+        truncated: bool = False,
+    ):
+        
+        self.n_step += 1
+        if self.baseline:
+            self.avg_reward += (reward - self.avg_reward)/self.n_step
+        probabilities = self._get_action_probability(observation)
+        self.preferences[observation] -= self.step_size*(reward - self.avg_reward)*probabilities[action]
+        self.preferences[observation][action] += self.step_size*(reward - self.avg_reward)
